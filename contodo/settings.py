f0ba0248d40ff63b177fad3773b846a58a243505
Mahmudul Alam <mahmudula2000@gmail.com>
2023-03-20 12:30:34 +0600

    added allowed host and disabled Debugging

--- contodo/settings.py
+++ contodo/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') or False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["contodo.mahmudul.cf"]
 
 # Application definition
 
