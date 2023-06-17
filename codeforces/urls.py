8fca1344f736fc6dcf972537a380e0d356335083
Mahmudul Alam <mahmudula2000@gmail.com>
2023-06-17 08:19:25 +0600

    replaced async function with sync

--- codeforces/urls.py
+++ codeforces/urls.py
@@ -7,5 +7,5 @@ app_name = 'codeforces'
 urlpatterns = [
     path('stat/', views.statistics, name='statistics'),
     path('update/', views.update_statistics, name='update_statistics'),
-    path('update-sync/', views.update_statistics, name='update_statistics_sync'),
+    path('update-sync/', views.update_statistics_sync, name='update_statistics_sync'),
 ]
